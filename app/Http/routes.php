a49f6a1c50f6d6c3ab1938f8830a892979ff463f
albert <albertxerta93@hotmail.com>
2016-02-26 20:07:00 +0100

    moar examples - Moved to Vehicles

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -31,78 +31,14 @@ Route::group(['middleware' => ['web']], function () {
 });
 
 
-//Dependency injection correcte
-
-//class Jeep
-//{
-//    public function __construct(Petrol $fuel)
-//    {
-//        $this->fuel = $fuel;
-//    }
-//
-//    public function refuel($litres)
-//    {
-//        return $litres * $this->fuel->getPrice();
-//    }
-//}
-//
-//class Petrol
-//{
-//    public function getPrice()
-//    {
-//        return 130.7;
-//    }
-//}
-//
-//$petrol = new Petrol;
-//$car = new JeepWrangler($petrol);
-//
-//$cost = $car->refuel(60);
-
-
-//DEPENDENCY INVERSION (D de SOLID)
-
-interface Fuel{
-    public function getPrice();
-}
-
-class Jeep
-{
-    public function __construct(Fuel $fuel)
-    {
-        $this->fuel = $fuel;
-    }
-
-    public function refuel($litres)
-    {
-        return $litres * $this->fuel->getPrice();
-    }
-}
-
-class Petrol implements Fuel
-{
-    public function getPrice()
-    {
-        return 1;
-    }
-}
-
-class Gasolina implements Fuel
-{
-    public function getPrice()
-    {
-        return 2;
-    }
-}
-
 
 //$gasolina = new Gasolina;
 //$car = new Jeep($gasolina);
 
-$car = $this->app->bind('Fuel', 'Petrol');
+$car = $this->app->bind('Fuel', '\App\Vehicle\Gasolina');
 
 
-$car = $this->app->make('Jeep');
+$car = $this->app->make('\App\Vehicle\Jeep');
 $cost = $car->refuel(60);
 
 echo $cost;
