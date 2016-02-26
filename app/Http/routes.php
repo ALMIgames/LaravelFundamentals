ead3e0d11523c3e3e5e7336015f15496db52b6ad
albert <albertxerta93@hotmail.com>
2016-02-26 19:10:43 +0100

    injeccio de dependencies exemple

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,3 +29,31 @@ Route::get('/', function () {
 Route::group(['middleware' => ['web']], function () {
     //
 });
+
+
+//Dependency injection
+class JeepWrangler
+{
+    public function __construct(Petrol $fuel)
+    {
+        $this->fuel = $fuel;
+    }
+
+    public function refuel($litres)
+    {
+        return $litres * $this->fuel->getPrice();
+    }
+}
+
+class Petrol
+{
+    public function getPrice()
+    {
+        return 130.7;
+    }
+}
+
+$petrol = new Petrol;
+$car = new JeepWrangler($petrol);
+
+$cost = $car->refuel(60);
